79fedd28821618fe5f1d91fffbf3c4981a17078f
Daniel Tkach <danieltkach@gmail.com>
2022-03-06 21:59:25 -0300

    first commit

---
+++ server/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const path = require('path');
+const AutoLoad = require('fastify-autoload');
+
+module.exports = async function (fastify, opts) {
+	fastify.register(AutoLoad, {
+		dir: path.join(__dirname, 'plugins'),
+		options: Object.assign({}, opts)
+	});
+
+	fastify.register(AutoLoad, {
+		dir: path.join(__dirname, 'routes'),
+		options: Object.assign({}, opts)
+	});
+};
